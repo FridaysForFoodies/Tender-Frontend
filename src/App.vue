9ccc8f72b1073ff2114b74e540e15b1fe295b7bf
Robin <robin.decker@student.htw-berlin.de>
2020-12-02 20:19:38 +0100

    new branch + updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
+    <Navigation />
     <router-view />
     <!-- Content -->
        <!-- Header -->
 
     <!-- Navigation -->
    
-    <Navigation />
+
   </div>
 </template>
 
@@ -22,12 +23,14 @@ export default {
 </script>
 
 <style>
+
 #app {
+  box-sizing: border-box;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #333333;
+  height: 100%;
 }
 </style>
---
+++ src/components/listing/RecipeComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="recipe-container shadow-lg mx-3 my-5 rounded-md">
+    <div class="recipe-image rounded-t-md" style="height:380px; background-size: cover" :style="{ backgroundImage: 'url(' + recipe.imageUrl + ')' }"></div>
+
+    <div class="recipe-info text-left p-5 flex flex-column items-center">
+      <div class="recipe-info-container w-full flex flex-column">
+
+        <div class="recipe-title h-auto">
+          <h2 class="text-2xl font-bold leading-6">
+            {{ recipe.title }}
+          </h2>
+
+          <small>
+            es fehlt dies das und das
+          </small>
+        </div>
+
+        <div class="recipe-favorite w-1/8">
+          <img alt="Settings" src="../../assets/images/heart.png">
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recipeComponent",
+  props: ['recipe'],
+}
+</script>
+
+<style scoped>
+  .recipe-info {
+    height: 90px;
+  }
+</style>
