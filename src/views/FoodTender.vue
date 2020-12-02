6f566cb5e2e82762ce109d211af3a196ab041a30
Josephine Battiston <josephinebattiston@web.de>
2020-12-02 18:10:21 +0100

    added routing and view components

---
+++ src/views/FoodTender.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1 >Food Tender</h1> 
+
+    <button @click="goBack" class="text-purple-600" >
+      &larr; Back to Home Search 
+    </button> 
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "FoodTender",
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1 >Home</h1> 
+      
+      <router-link :to=" { name: 'FoodTender'}" class="text-purple-600" > 
+        Go To Food Tinder Screen   &rarr;
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
