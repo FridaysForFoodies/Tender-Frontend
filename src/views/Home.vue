7777d1417f45ed06c51fa835707d21b8acb47a09
Josephine Battiston <josephinebattiston@web.de>
2020-12-04 13:08:36 +0100

    added Home to josephine dev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1 >Home</h1> 
+      <h1> Home </h1> 
       
       <router-link :to=" { name: 'FoodTender'}" class="text-purple-600" > 
         Go To Food Tinder Screen   &rarr;
