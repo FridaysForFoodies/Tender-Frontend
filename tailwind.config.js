68364786d220f9b08a2d26266b25f8a45b8791c6
Josephine Battiston <josephinebattiston@web.de>
2020-12-04 20:04:29 +0100

    added delete, dislike, like and back buttons added tender screen ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        darkGrex: "var(--color-dark-grey)",
+        lightGrey: "var(--color-light-grey)"
+      }
+    },
   },
   variants: {
     extend: {},
