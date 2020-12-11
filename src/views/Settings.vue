8d5c32b95b5292af878da7afe5d987b5e184ca00
Andrej <andrej@loparev.it>
2020-12-11 17:48:26 +0100

    add slider shadow

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -10,7 +10,7 @@
         <label class="switch">
           <input type="checkbox" id="vegetarian" value="vegetarian" name="vegetarian" v-model="vegetarian"
                  @change="checkConsistency(false)">
-          <span class="slider round"/>
+          <span class="switcher round"/>
         </label>
       </div>
 
@@ -20,7 +20,7 @@
       <div class="text-right">
         <label class="switch">
           <input type="checkbox" id="vegan" name="vegan" value="vegan" v-model="vegan" @change="checkConsistency(true)">
-          <span class="slider round"/>
+          <span class="switcher round"/>
         </label>
       </div>
 
@@ -30,7 +30,7 @@
       <div class="text-right">
         <label class="switch">
           <input type="checkbox" id="gluten" name="gluten" value="gluten" v-model="gluten">
-          <span class="slider round"/>
+          <span class="switcher round"/>
         </label>
       </div>
 
@@ -41,14 +41,17 @@
         <label class="switch">
           <input type="checkbox" id="dairy" name="dairy" value="dairy" v-model="dairy"
                  @change="checkConsistency(false)">
-          <span class="slider round"/>
+          <span class="switcher round"/>
         </label>
       </div>
     </div>
 
-    <label for="time">Cooking time: {{ time }}<span v-show="time == 60">+</span></label>
+    <div class="slider-container">
+    <label class="text-left font-bold" for="time">Cooking time: {{ time }}<span v-show="time == 60">+</span></label>
     <br>
-    <input type="range" min="15" max="60" value="30" step="15" id="time" v-model="time" name="time">
+    <input type="range" min="15" max="60" value="30" step="15" id="time" v-model="time" name="time" class="slider">
+    </div>
+
   </div>
 </template>
 
@@ -73,11 +76,19 @@ export default {
         this.vegan = false
       }
     }
+  },
+  computed: {
+
   }
 }
 </script>
 
 <style scoped>
+
+/*
+ * checkbox style
+ */
+
 .switch {
   position: relative;
   display: inline-block;
@@ -91,7 +102,7 @@ export default {
   height: 0;
 }
 
-.slider {
+.switcher {
   position: absolute;
   cursor: pointer;
   top: 0;
@@ -103,7 +114,7 @@ export default {
   transition: .4s;
 }
 
-.slider:before {
+.switcher:before {
   position: absolute;
   content: "";
   height: 26px;
@@ -115,26 +126,71 @@ export default {
   transition: .4s;
 }
 
-input:checked + .slider {
+input:checked + .switcher {
   background-color: var(--color-primary);
 }
 
-input:focus + .slider {
+input:focus + .switcher {
   box-shadow: 0 0 1px var(--color-primary);
 }
 
-input:checked + .slider:before {
+input:checked + .switcher:before {
   -webkit-transform: translateX(26px);
   -ms-transform: translateX(26px);
   transform: translateX(26px);
 }
 
 /* Rounded sliders */
-.slider.round {
+.switcher.round {
   border-radius: 34px;
 }
 
-.slider.round:before {
+.switcher.round:before {
   border-radius: 50%;
 }
+
+/*
+ * range slider style
+ */
+
+.slider-container {
+  width: 100%;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 3px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background: #FFF;
+  cursor: pointer;
+  /*box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;*/
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #FFF;
+  cursor: pointer;
+}
+
 </style>
