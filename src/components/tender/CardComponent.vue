1a389ca5a315ff84eef5ab71a1e3d4b81e6d9d30
Robin <robin.decker@student.htw-berlin.de>
2020-12-18 13:39:49 +0100

    improved swipe ui

--- src/components/tender/CardComponent.vue
+++ src/components/tender/CardComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- Draggable -->
   <Vue2InteractDraggable
-      class="absolute flex flex-col left-0 right-0 top-0 h-full mx-6 transform rounded-lg bg-gray-100"
+      class="dragging-container absolute flex flex-col left-0 right-0 top-0 h-full mx-6 transform rounded-lg bg-gray-100 bg-cover bg-no-repeat"
+      style="background-image: url('https://image.freepik.com/fotos-kostenlos/hoher-winkel-der-koestlichen-ramen-in-der-schuessel_23-2148678758.jpg')"
       :interact-block-drag-down="interactBlockDragDown"
       :interact-block-drag-left="interactBlockDragLeft"
       :interact-block-drag-right="interactBlockDragRight"
@@ -16,17 +17,19 @@
       @draggedRight="like(item)"
   >
     <!-- Title -->
-    <div class="flex-1"> 
-      {{ item.title }} {{ this.$store.state.count }}
+    <div class="flex-1">
+      <h2 class="text-center font-oswald text-6xl uppercase" style="line-height: 8;">
+        {{ item.title }}
+      </h2>
     </div>
     
     <!-- Buttons need to be moved to card stack component -->
-    <div class="flex flex-row justify-around w-full h-16 text-center mb-6">
-      <button class="w-16 h-auto" @click="dislike(item)">
+    <div class="flex flex-row justify-around w-full h-24 text-center mb-6">
+      <button class="rounded-full h-24 w-24 flex items-center justify-center bg-white border-8 border-gray-200 p-4" @click="dislike(item)">
         <img class="transform rotate-180" alt="Dislike" src="../../assets/images/dislike.png">
       </button>
 
-      <button class="w-16 h-auto" @click="like(item)">
+      <button class="rounded-full h-24 w-24 flex items-center justify-center bg-white border-8 border-gray-200 p-4" @click="like(item)">
         <img alt="Like" src="../../assets/images/like.png">
       </button>
     </div>
@@ -74,5 +77,19 @@ export default {
 </script>
 
 <style scoped>
+.dragging-container,
+.dragging-container * {
+  -ms-touch-action: none;
+  touch-action: none;
+}
 
+.dragging-container:before {
+  content: "";
+  position: absolute;
+  left: 0; right: 0;
+  top: 0; bottom: 0;
+  background: rgba(255,255,255,.7);
+  border-radius: 0.5rem;
+  z-index: -1;
+}
 </style>
