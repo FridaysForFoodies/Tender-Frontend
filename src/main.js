bd43b6a6f7dd702ddeb518d73aba78a55ad52020
lenasrd <lena.srd@hotmail.de>
2020-12-18 19:03:36 +0100

    add apollo and create example

--- src/main.js
+++ src/main.js
@@ -6,15 +6,18 @@ import './assets/css/app.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHeart, faCog, faPlay } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
- 
+import { createProvider } from './vue-apollo'
+require('dotenv').config()
+
 library.add(faHeart, faCog, faPlay)
- 
+
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store, 
-  render: h => h(App),
+  store,
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
