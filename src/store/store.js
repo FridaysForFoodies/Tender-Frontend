5a8682d37dc965982a1f66546d0f196aacd7e1dd
Josephine Battiston <josephinebattiston@web.de>
2021-01-07 19:04:40 +0100

    recipe tags test

--- src/store/store.js
+++ src/store/store.js
@@ -71,9 +71,7 @@ const store = new Vuex.Store({
     }, 
     actions: {
       // Instead of mutating the state, actions commit mutations.
-
-      // Actions can contain arbitrary >> asynchronous operations << . --> APOLLO Stuff goes in here :)   
-
+      // Actions can contain arbitrary >> asynchronous operations << . --> APOLLO Stuff goes in here :)
     }
   })
 
