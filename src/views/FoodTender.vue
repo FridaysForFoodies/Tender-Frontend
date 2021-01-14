37bcc20e1ae40ac1c8e7f9b76b2b87ee5db2848a
Josephine Battiston <josephinebattiston@web.de>
2021-01-14 20:58:06 +0100

    css fixes

--- src/views/FoodTender.vue
+++ src/views/FoodTender.vue
@@ -33,7 +33,7 @@
     />
   </div>
     <!-- Buttons need to be moved to card stack component -->
-    <div class="flex justify-around w-full h-24 text-center mb-1 mt-1 pt-4">
+    <div class="flex justify-around w-full h-24 text-center mb-28 mt-4">
       <button class="rounded-full h-24 w-24 items-center justify-center bg-white border-8 border-gray-200 p-4" @click="disliked(item, index)">
         <img class="transform rotate-180 " alt="Dislike" src="../assets/images/dislike.png">
       </button>
